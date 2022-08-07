0e5daa2e47b61a46e026d608ff851766661f38ef
Maxwell Mao <maodihui@foxmail.com>
2022-08-07 21:33:05 +0800

    Initial commit

---
+++ src/main/java/org/maxwell/threads/VolatieDemo.java
@@ -0,0 +1,39 @@
+package org.maxwell.threads;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @description:
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/7/12 0:40
+ */
+public class VolatieDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        VolatileDemo demo = new VolatileDemo();
+
+        for (int i = 0; i < 5; i++) {
+            Thread t = new Thread(demo);
+            t.start();
+        }
+
+        Thread.sleep(1000);
+        System.out.println(demo.count);
+    }
+
+    static class VolatileDemo implements Runnable {
+//        public volatile int count;
+        public volatile AtomicInteger count = new AtomicInteger(0);
+
+        public void run() {
+            addCount();
+        }
+
+        public void addCount() {
+            for (int i = 0; i < 10000; i++) {
+                count.incrementAndGet();
+            }
+        }
+    }
+}
